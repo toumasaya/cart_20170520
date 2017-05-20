0923c1e8b4afccca6f7f6e1248c489a65fb2bd28
toumasaya <luffy.w.pirate@gmail.com>
2017-05-20 16:12:34 +0800

    cart test: it will be a Product

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -23,4 +23,19 @@ RSpec.describe Cart, type: :model do
       expect(cart.items.last.quantity).to be 3
     end
   end
+
+  it "will be a Product" do
+    p1 = Product.create(name: "P1")
+    p2 = Product.create(name: "P2")
+    cart = Cart.new
+
+    # p p1
+    # p p2
+
+    2.times { cart.add_item(p1.id) }
+    3.times { cart.add_item(p2.id) }
+
+    expect(cart.items.first.product).to be_kind_of Product
+    expect(cart.items.last.product_id).to be p2.id
+  end
 end
