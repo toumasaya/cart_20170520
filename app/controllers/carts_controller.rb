2d3aaee3d3026cc9dbb3500258fe445b3342dfab
toumasaya <luffy.w.pirate@gmail.com>
2017-05-21 12:30:29 +0800

    Add Cart resources

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,14 @@
+class CartsController < ApplicationController
+  def add
+    # 按下 add_cart_path , 把 cart 送進 session 裡
+    # cart = Cart.new # 每次都會新建一台購物車，這樣變成每次點擊都會是新的，無法加入項目
+    # 應該是要先去把 session 裡的 hash 抓回購物車（如果有東西的話）
+    cart = Cart.from_hash(session[:cart9527])
+    cart.add_item(params[:id])
+
+    session[:cart9527] = cart.to_hash
+
+    # debugger
+    redirect_to products_path, success: "Add to cart successfully"
+  end
+end
