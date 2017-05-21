9097dc3bc4c559696e349f3e7134063e09e7c8a6
toumasaya <luffy.w.pirate@gmail.com>
2017-05-21 11:43:53 +0800

    Add flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  add_flash_types :success
+  add_flash_types :warning
+  add_flash_types :danger
+  add_flash_types :info
 end
