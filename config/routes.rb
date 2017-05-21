e928e99487d2a4a89ab37ca5d8f6646b113bc3da
toumasaya <luffy.w.pirate@gmail.com>
2017-05-21 12:08:25 +0800

    Add cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "products#index"
 
   resources :products
+  resource :cart, only: [:show, :destroy] do
+    collection do
+      post :add, path: "/add/:id"
+      get :checkout
+    end
+  end
 end
