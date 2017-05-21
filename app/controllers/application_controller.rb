76c3ab27e2c4018591317b12c464272db72d13a8
toumasaya <luffy.w.pirate@gmail.com>
2017-05-21 13:43:21 +0800

    Add init_cart to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # 所有 controller 都會執行
+  before_action :init_cart
+
   add_flash_types :success
   add_flash_types :warning
   add_flash_types :danger
   add_flash_types :info
+
+  private
+
+  # 把這個方法從 add action 取出來，放在這裡
+  def init_cart
+    @cart = Cart.from_hash(session[:cart9527])
+  end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,12 @@
 class CartsController < ApplicationController
+  # before_action :init_cart
+
   def add
     # 按下 add_cart_path , 把 cart 送進 session 裡
     # cart = Cart.new # 每次都會新建一台購物車，這樣變成每次點擊都會是新的，無法加入項目
     # 應該是要先去把 session 裡的 hash 抓回購物車（如果有東西的話）
-    cart = Cart.from_hash(session[:cart9527])
+    # cart = Cart.from_hash(session[:cart9527])
+    # init_cart
     cart.add_item(params[:id])
 
     session[:cart9527] = cart.to_hash
